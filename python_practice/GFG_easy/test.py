ccb88820486a5d6acbda55a95533c795758b384f
sairanjith101 <sairanjith101@gmail.com>
2024-12-23 10:55:25 +0530

    Added

--- python_practice/GFG_easy/test.py
+++ python_practice/GFG_easy/test.py
@@ -1,11 +1,11 @@
-import math
+k = 16
+arr = [9, 7, 16, 16, 4]
 
-def lcm_and_gcd(a,b):
-    gcd = math.gcd(a,b)
-    lcm = (a*b) // gcd
-    return [lcm, gcd]
+def search(k, arr):
+    for i in range(len(arr)):
+        if arr[i] == k:
+            return i+1
+    else:
+        return -1
 
-a = 5
-b = 10
-
-print(lcm_and_gcd(a,b))
+print(search(k, arr))
