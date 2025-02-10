ed7ff5c5d3719a5c195ade7a0e3ed2074fd524aa
sairanjith101 <sairanjith101@gmail.com>
2025-02-10 15:32:15 +0530

    add

--- django_practice/DRF/Demo_1/BillingSystem/Inventory/views.py
+++ django_practice/DRF/Demo_1/BillingSystem/Inventory/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import *
 
-# Create your views here.
+class ProductsView(APIView):
+    def post(self, request):
+        new_product = Products(product_name = request.data["product_name"], code = request.data["code"], price = request.data["price"])
+
+        new_product.save()
+        
+        return Response("Data Saved") 
