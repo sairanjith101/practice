c5fed4a0848eda676ac80264cf692bfd777367b1
sairanjith101 <sairanjith101@gmail.com>
2025-02-18 10:09:43 +0530

    add

--- django_practice/DRF/Demo_1/BillingSystem/Inventory/serializers.py
+++ django_practice/DRF/Demo_1/BillingSystem/Inventory/serializers.py
@@ -1,9 +1,17 @@
 from rest_framework import serializers
 from .models import *
 
+# All fields
 class Products_Serializers(serializers.ModelSerializer):
     class Meta:
 
         model = Products
         fields = '__all__'
+
+# Single Field
+class Products_Serializers2(serializers.ModelSerializer):
+    class Meta:
+
+        model = Products
+        fields = ['product_name']
         
--- django_practice/DRF/Demo_1/BillingSystem/Inventory/views.py
+++ django_practice/DRF/Demo_1/BillingSystem/Inventory/views.py
@@ -1,20 +1,37 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import *
+from .serializers import *
 
 class ProductsView(APIView):
+
+    # Without Serializer for all products
+    # def get(self, request):
+    #     all_products = Products.objects.all()
+    #     products_data = []
+    #     for product in all_products:
+    #         single_product = {
+    #             "id": product.id,
+    #             "product_name": product.product_name,
+    #             "code": product.code,
+    #             "price": product.price
+    #         }
+    #         products_data.append(single_product)
+    #     return Response(products_data)
+    
+    # Serializer for all products
+    # def get(self, request):
+    #     all_products = Products.objects.all()
+    #     serialized_products = Products_Serializers(all_products, many=True).data
+    #     print(serialized_products)
+    #     return Response(serialized_products)
+    
+    # Serializer for single product
     def get(self, request):
         all_products = Products.objects.all()
-        products_data = []
-        for product in all_products:
-            single_product = {
-                "id": product.id,
-                "product_name": product.product_name,
-                "code": product.code,
-                "price": product.price
-            }
-            products_data.append(single_product)
-        return Response(products_data)
+        serialized_products = Products_Serializers2(all_products, many=True).data
+        print(serialized_products)
+        return Response(serialized_products)
     
     def post(self, request):
         new_product = Products(product_name = request.data["product_name"], code = request.data["code"], price = request.data["price"])
@@ -25,15 +42,22 @@ class ProductsView(APIView):
     
 
 class ProductsViewById(APIView):
+    # without serializer for single product
+    # def get(self,request,id):
+    #     product = Products.objects.get(id=id)
+    #     single_product = {
+    #         "id": product.id,
+    #         "product_name": product.product_name,
+    #         "code": product.code,
+    #         "price": product.price
+    #         }
+        
+    #     return Response(single_product)
+    
+    # with serializer for single product
     def get(self,request,id):
         product = Products.objects.get(id=id)
-        single_product = {
-            "id": product.id,
-            "product_name": product.product_name,
-            "code": product.code,
-            "price": product.price
-            }
-        
+        single_product = Products_Serializers2(product).data        
         return Response(single_product)
 
     def patch(self, request, id):
