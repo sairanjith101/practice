96fc2ba0110a079a8973a8c3d041445aa40551f5
sairanjith101 <sairanjith101@gmail.com>
2025-03-10 11:36:33 +0530

    add

--- python_practice/extra_qus/test.py
+++ python_practice/extra_qus/test.py
@@ -1,3 +1,26 @@
-lower = 900
-upper = 1000
+arr = [
+[1,2,3,4],
+[5,6,7,8],
+[1,2,3,4],
+[5,6,7,8]
+]
 
+sum_1 = 0
+sum_3 = 0
+
+for i in range(len(arr)):
+    sum_1 += arr[i][0]
+    sum_3 += arr[i][2]
+
+print(sum_1)
+print(sum_3)
+
+mul_2 = 1
+mul_4 = 1
+
+for i in range(len(arr)):
+    mul_2 *= arr[i][1]
+    mul_4 *= arr[i][3]
+
+print(mul_2)
+print(mul_4)
