24d41ced6e52e54f385dc69b3b0f8a0364da46fc
sairanjith101 <sairanjith101@gmail.com>
2025-03-13 10:06:24 +0530

    add

--- python_practice/file_handling_qus/FH_easy/test.py
+++ python_practice/file_handling_qus/FH_easy/test.py
@@ -1,8 +1,3 @@
-num_of_lines = 0
-
 with open('newfile.txt', 'r') as file1:
     lines = file1.readlines()
-    for line in lines:
-        num_of_lines += 1
-
-print(num_of_lines)
+    print(lines[-1].strip())
